d0a94a0d64f984df4a55c868748d430f2f2ec104
swatheeswai <swathiswari83@gmail.com>
2025-03-11 17:37:29 +0530

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import com.viewClass.UserView;
+
+public class Main {
+    public static void main(String[] args) {
+        UserView userView = new UserView();
+        userView.showUserView();
+    }
+}
+
